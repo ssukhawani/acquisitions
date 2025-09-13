0293c18ea738568bc450b19efa625b8ab79118ad
sahil-sukhwani <sahil@shurutech.com>
2025-09-13 14:02:22 +0530

    setup neon postgress with drizzle orm

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon(process.env.DB_URL);
+
+const db = drizzle(sql, { logger: true });
+
+export { db, sql };
